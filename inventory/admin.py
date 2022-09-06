c4c288d6c6ca7e4b8f54b19839a6c4b55a681f10
camilo1rp <camilo1rp@gmail.com>
2022-09-06 15:47:11 -0500

    changed admin for displaying registrarion inline

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from inventory.models import BulkRegistration, ItemRegistration, ItemRegistrationChange
 
-admin.site.register(BulkRegistration)
-admin.site.register(ItemRegistration)
 admin.site.register(ItemRegistrationChange)
+
+
+class ItemRegistrationInline(admin.TabularInline):
+    model = ItemRegistration
+
+
+class BulkRegistrationAdmin(admin.ModelAdmin):
+    readonly_fields = ["user", "cost", "product_quantity", "quantity"]
+    inlines = [
+        ItemRegistrationInline,
+    ]
+
+admin.site.register(BulkRegistration, BulkRegistrationAdmin)
